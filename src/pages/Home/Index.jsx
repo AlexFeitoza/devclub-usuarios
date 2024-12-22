07251ca083a380fc08f6e9525f12d9b8de80072b
Alex Feitoza <159736255+AlexFeitoza@users.noreply.github.com>
2024-12-21 22:06:21 -0300

    useref

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,9 +1,19 @@
+import { useRef } from "react"
+
 import { Container, Title, TopBackground, Form, ContainerInputs, Input, InputLabel, Button } from "./styles"
 
 import UsersImage from '../../assets/users.png'
 
+
 function Home() {
 
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
+
+  function registerNewUser() {
+    console.log(inputName.current.value, inputAge.current.value, inputEmail.current.value)
+  }
 
   return (
     <>
@@ -22,14 +32,14 @@ function Home() {
                 <InputLabel>
                   Nome <span>*</span>
                 </InputLabel>
-                <Input type="text" placeholder="Nome do usuário" />
+                <Input type="text" placeholder="Nome do usuário" ref={inputName} />
               </div>
 
               <div>
                 <InputLabel>
                   Idade <span>*</span>
                 </InputLabel>
-                <Input type="number" placeholder="Idade do usuario" />
+                <Input type="number" placeholder="Idade do usuario" ref={inputAge}/>
               </div>
             
 
@@ -39,12 +49,12 @@ function Home() {
             <InputLabel>
               E-mail <span>*</span>
             </InputLabel>
-            <Input type="email" placeholder="E-mail do usuário" />
+            <Input type="email" placeholder="E-mail do usuário" ref={inputEmail}/>
           </div>
 
 
 
-          <Button>Cadastrar Usuário</Button>
+          <Button type='button' onClick={registerNewUser}>Cadastrar Usuário</Button>
         </Form>
       </Container>
     </>
