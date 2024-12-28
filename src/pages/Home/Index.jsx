73fef7f9f6af09a902b017741f3f22c39e23e7fb
Alex Feitoza <159736255+AlexFeitoza@users.noreply.github.com>
2024-12-27 23:02:09 -0300

    useEffect

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,4 +1,5 @@
 import { useRef } from "react"
+import { useNavigate } from "react-router-dom"
 import api from "../../services/api"
 
 
@@ -10,13 +11,13 @@ import TopBackground from "../../components/TopBackground"
 
 
 
-
-
 function Home() {
 
   const inputName = useRef()
   const inputAge = useRef()
   const inputEmail = useRef()
+
+  const navigate = useNavigate()
 
   async function registerNewUser() {
     const data = await api.post('/usuarios', {
@@ -68,7 +69,7 @@ function Home() {
           <Button type='button' onClick={registerNewUser} theme="primary">Cadastrar Usuário</Button>
         </Form>
 
-        <Button type='button'>Lista de Usuários</Button>
+        <Button type='button' onClick={() => navigate('/lista-de-usuarios')}>Lista de Usuários</Button>
 
 
       </Container>
--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -1,17 +1,34 @@
+import api from '../../services/api'
+
 import Button from "../../components/Button"
+import { useNavigate } from "react-router-dom"
 import TopBackground from "../../components/TopBackground"
+import { useEffect } from "react";
 
-function ListUsers(){
 
-    return(
-            <>
-                <TopBackground />
-                <h1>
-                    Listagem de Usuários
-                </h1>
-                
-                <Button>Back</Button>
-            </>
+
+function ListUsers() {
+
+    // Toda vez que a tela carrega, o useEffect é chamado
+    // Toda vez que uma variável Muda de valor, ele é chamado
+
+    useEffect(() => {
+
+        async function getUsers() {
+            const usersFromApi = await api.get('/usuarios')
+            console.log(usersFromApi)
+        }
+        getUsers()
+    }, []);
+
+    const navigate = useNavigate()
+
+    return (
+        <>
+            <TopBackground />
+            <h1> Listagem de Usuários</h1>
+            <Button type='button' theme={'primary'} onClick={() => navigate('/')}>Back</Button>
+        </>
     )
 }
 
