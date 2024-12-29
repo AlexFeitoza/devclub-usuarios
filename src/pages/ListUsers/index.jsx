250a4feeedd390fe19b392cf895ec5dad032ac27
Alex Feitoza <159736255+AlexFeitoza@users.noreply.github.com>
2024-12-28 22:30:29 -0300

    css

--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -1,35 +1,54 @@
-import api from '../../services/api'
+import api from "../../services/api";
 
-import Button from "../../components/Button"
-import { useNavigate } from "react-router-dom"
-import TopBackground from "../../components/TopBackground"
-import { useEffect } from "react";
+import Button from "../../components/Button";
+import { useNavigate } from "react-router-dom";
+import TopBackground from "../../components/TopBackground";
+import { useEffect, useState } from "react";
+import Trash from '../../assets/trash.svg'
 
+import { CardUsers, Container, ContainerUsers, Title, TrashIcon,  } from "./styles";
 
 
 function ListUsers() {
+  // Toda vez que a tela carrega, o useEffect é chamado
+  // Toda vez que uma variável Muda de valor, ele é chamado
 
-    // Toda vez que a tela carrega, o useEffect é chamado
-    // Toda vez que uma variável Muda de valor, ele é chamado
+  const [users, setUsers] = useState([]);
 
-    useEffect(() => {
+  useEffect(() => {
+    async function getUsers() {
+      const { data } = await api.get("/usuarios");
 
-        async function getUsers() {
-            const usersFromApi = await api.get('/usuarios')
-            console.log(usersFromApi)
-        }
-        getUsers()
-    }, []);
+      setUsers(data);
+    }
+    getUsers();
+  }, []);
 
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    return (
-        <>
-            <TopBackground />
-            <h1> Listagem de Usuários</h1>
-            <Button type='button' theme={'primary'} onClick={() => navigate('/')}>Back</Button>
-        </>
-    )
+  return (
+    <Container>
+      <TopBackground />
+      <Title>Lista de Usúarios</Title>
+
+      <ContainerUsers >
+        {users.map((user) => (
+        <CardUsers key={user.id}>
+          <div>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+            <p>{user.age}</p>
+          </div>
+          <TrashIcon src={Trash} alt="lixeira" />
+          </CardUsers>
+        ))}
+      </ContainerUsers>
+
+      <Button type="button" theme={"primary"} onClick={() => navigate("/")}>
+        Back
+      </Button>
+    </Container>
+  );
 }
 
-export default ListUsers
+export default ListUsers;
