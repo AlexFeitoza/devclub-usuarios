f7499f00b25bf9f752d15d9d90b034b840e9aa2c
Alex Feitoza <159736255+AlexFeitoza@users.noreply.github.com>
2024-12-29 10:14:02 -0300

    css end

--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -6,49 +6,52 @@ import TopBackground from "../../components/TopBackground";
 import { useEffect, useState } from "react";
 import Trash from '../../assets/trash.svg'
 
-import { CardUsers, Container, ContainerUsers, Title, TrashIcon,  } from "./styles";
+
+import { AvatarProfile, CardUsers, Container, ContainerUsers, Title, TrashIcon, } from "./styles";
 
 
 function ListUsers() {
-  // Toda vez que a tela carrega, o useEffect é chamado
-  // Toda vez que uma variável Muda de valor, ele é chamado
+    // Toda vez que a tela carrega, o useEffect é chamado
+    // Toda vez que uma variável Muda de valor, ele é chamado
 
-  const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    async function getUsers() {
-      const { data } = await api.get("/usuarios");
+    useEffect(() => {
+        async function getUsers() {
+            const { data } = await api.get("/usuarios");
 
-      setUsers(data);
-    }
-    getUsers();
-  }, []);
+            setUsers(data);
+        }
+        getUsers();
+    }, []);
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  return (
-    <Container>
-      <TopBackground />
-      <Title>Lista de Usúarios</Title>
+    return (
+        <Container>
+            <TopBackground />
+            <Title>Lista de Usúarios</Title>
 
-      <ContainerUsers >
-        {users.map((user) => (
-        <CardUsers key={user.id}>
-          <div>
-            <p>{user.name}</p>
-            <p>{user.email}</p>
-            <p>{user.age}</p>
-          </div>
-          <TrashIcon src={Trash} alt="lixeira" />
-          </CardUsers>
-        ))}
-      </ContainerUsers>
+            <ContainerUsers >
+                {users.map((user) => (
+                    <CardUsers key={user.id}>
+                        <AvatarProfile src={`https://avatar.iran.liara.run/public?username=${user.id}`} alt="avatar-user"/>
+                        <div>
+                            <h3>{user.name}</h3>
+                            <p>{user.age}</p>
+                            <p>{user.email}</p>
+                            
+                        </div>
+                        <TrashIcon src={Trash} alt="lixeira" />
+                    </CardUsers>
+                ))}
+            </ContainerUsers>
 
-      <Button type="button" theme={"primary"} onClick={() => navigate("/")}>
-        Back
-      </Button>
-    </Container>
-  );
+            <Button type="button" theme={"primary"} onClick={() => navigate("/")}>
+                Back
+            </Button>
+        </Container>
+    );
 }
 
 export default ListUsers;
--- src/pages/ListUsers/styles.js
+++ src/pages/ListUsers/styles.js
@@ -1,11 +1,74 @@
 import styled from "styled-components";
 
-export const Container = styled.div ``
+export const Container = styled.div`
+    background-color:  #181f36;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+    padding: 20px;
+`
 
-export const Title = styled.h2 ``
+export const Title = styled.h2`
+    color: #fff;
+    text-align: center;
+    font-size: 38px;
+    font-style: normal;
+    font-weight: 600;
+    margin-top: 40px;
+`
 
-export const ContainerUsers = styled.div ``
+export const ContainerUsers = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 10px;
+    margin: 40px 0;
 
-export const CardUsers = styled.div ``
+    @media (max-width: 750px){
+        grid-template-columns: 1fr;
+    }
+`
 
-export const TrashIcon = styled.img ``
+export const CardUsers = styled.div`
+    background-color: #252d48;
+    padding: 16px;
+    border-radius: 12px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 20px;
+    max-width: 380px;
+
+    h3 {
+        color: #fff;
+        font-size: 22px;
+        margin-bottom: 3px;
+        text-transform: capitalize;
+    }
+
+    p {
+        color: #fff;
+        font-size: 14px;
+        font-weight: 400;
+    }
+   
+`
+
+export const TrashIcon = styled.img`
+    cursor: pointer;
+    padding-left: 30px;
+
+    &:hover {
+        opacity: 0.5;
+    }
+
+    &:active {
+        opacity: 0.2;
+    }
+`
+
+export const AvatarProfile = styled.img`
+    width: 70px;
+
+`
